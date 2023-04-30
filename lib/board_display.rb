49ad01abbdf7cb4d11a05121cbca63d8fba2797e
D.J. Crosby <taladan@gmail.com>
2023-04-29 20:18:13 -0400

    feat: add piece placement on board
    display piece on board in correct color (to be able to see piece).
    Still buggy and putting piece in wrong place on board.  Have to
    figure out how to apply math to the numbers in CURRENTLY_OCCUPIED
    to get the piece to display in the right square.

--- lib/board_display.rb
+++ lib/board_display.rb
@@ -1,7 +1,10 @@
 require 'colorize'
+require 'pry-byebug'
 
 # Handle display of chess board of `n x m` size.  Default size is 8 x 8
 class BoardDisplay
+  attr_accessor :CURRENTLY_OCCUPIED
+
   def initialize(rows = 8, columns = 8)
     @ROWS = rows
     @COLUMNS = columns
@@ -9,38 +12,47 @@ class BoardDisplay
     @EMPTY_PIXEL = '      '
     @OCCUPIED_PIXEL = '  K   '
     @EMPTY_SCAN = Array.new(@COLUMNS, @EMPTY_PIXEL)
+    @ALPHA = ('a'..'bb').to_a
+    @CURRENTLY_OCCUPIED = [0, 0]
   end
+  # @EMPTY_SCAN[@CURRENTLY_OCCUPIED[0]] = @OCCUPIED_PIXEL if row == @CURRENTLY_OCCUPIED[1]
 
   # Display current state of chess board
   def display
     @ROWS.times do |row|
-      puts build_row(@EMPTY_SCAN, row + 1, false) if row.even?
-      puts build_row(@EMPTY_SCAN, row + 1, true) if row.odd?
+      puts build_row(@EMPTY_SCAN, row, false) if row.even?
+      puts build_row(@EMPTY_SCAN, row, true) if row.odd?
     end
+    print '  ' * @ROWS.to_s.length
+    @COLUMNS.times do |column|
+      print "  #{@ALPHA[column]}   "
+    end
+    print "\n"
   end
 
   private
 
-  #  builds 1/3 of a row of cells
+  # Build scan string (white/black/white/black)
   def build_scan(pixels)
     array = []
     pixels.each_with_index do |pixel, index|
       array << if index.even?
-                 pixel.on_white
+                 pixel.black.on_white
                else
-                 pixel.on_black
+                 pixel.white.on_black
                end
     end
     array
   end
 
+  # Build alternate scan string (black/white/black/white)
   def flip_squares(pixels)
     array = []
     pixels.each_with_index do |pixel, index|
       array << if index.even?
-                 pixel.on_black
+                 pixel.white.on_black
                else
-                 pixel.on_white
+                 pixel.black.on_white
                end
     end
     array
@@ -51,10 +63,15 @@ class BoardDisplay
     row = ''
     counter = 0
     while counter < @SQUARE_HEIGHT
+      pixels[@CURRENTLY_OCCUPIED[0]] = if row_number == @CURRENTLY_OCCUPIED[1] && counter == 1
+                                         @OCCUPIED_PIXEL
+                                       else
+                                         @EMPTY_PIXEL
+                                       end
       if row_number.to_s.length < 2
-        row += "#{row_number}" + ' ' * (@ROWS.to_s.length + 1) if counter.odd?
+        row += "#{@ROWS - row_number}" + ' ' * (@ROWS.to_s.length + 1) if counter.odd?
       elsif row_number.to_s.length >= 2
-        row += "#{row_number}" + ' ' * @ROWS.to_s.length if counter.odd?
+        row += "#{@ROWS - row_number}" + ' ' * @ROWS.to_s.length if counter.odd?
       end
       row += ' ' * (@ROWS.to_s.length + 2) if counter.even?
       row += flip_squares(pixels).join if alternate
