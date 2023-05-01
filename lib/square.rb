3d0006cf3d6e3c91181777ac390229e9aa486e3c
D.J. Crosby <taladan@gmail.com>
2023-05-01 08:27:21 -0400

    typo

--- lib/square.rb
+++ lib/square.rb
@@ -10,7 +10,7 @@ class Square
 
   def initialize(name)
     @name = name
-    @neighbors_position = {}
+    @neighbors_positions = {}
     @neighbors_name = []
     @position = []
     @contents = nil
