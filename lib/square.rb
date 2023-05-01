2ff496bbd633f5c69ced86ccc53faa20d1e3546c
D.J. Crosby <taladan@gmail.com>
2023-05-01 07:51:21 -0400

    initial commit
    implement graph node object that will act as square on chessboard

---
+++ lib/square.rb
@@ -0,0 +1,18 @@
+# square.rb
+
+# Creates a graph node named 'square'
+class Square
+  attr_accessor :name,
+                :neighbors_position,
+                :neighbors_name,
+                :position,
+                :contents
+
+  def initialize(name)
+    @name = name
+    @neighbors_position = []
+    @neighbors_name = []
+    @position = []
+    @contents = nil
+  end
+end
