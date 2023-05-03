5a640d4ed5d0ff355057399bf5490ee2030b2147
D.J. Crosby <taladan@gmail.com>
2023-05-03 10:58:40 -0400

    fix: refactor neighbor edge tracking
    Implement a neighbors hash to allow for better edge tracking by name and
    position.  hash keys are shortened compass directions as they relate
    to the position of the square on the board.
    
    North       -> `n`
    North East  -> `ne`
    East        -> `e`
    South East  -> `se`
    South       -> `s`
    South West  -> `sw`
    West        -> `w`
    North West  -> `nw`

--- lib/square.rb
+++ lib/square.rb
@@ -1,5 +1,6 @@
 # square.rb
 require 'colorize'
+require_relative 'display'
 
 # Creates a graph node named 'square'
 class Square
@@ -13,25 +14,38 @@ class Square
     @name = name
     @neighbors_positions = {}
     @neighbors_name = []
+    @neighbors = {
+      n: { name: nil,
+           position: nil },
+      ne: { name: nil,
+            position: nil },
+      e: { name: nil,
+           position: nil },
+      se: { name: nil,
+            position: nil },
+      s: { name: nil,
+           position: nil },
+      sw: { name: nil,
+            position: nil },
+      w: { name: nil,
+           position: nil },
+      nw: { name: nil,
+            position: nil }
+    }
     @position = []
     @contents = nil
+    @color = nil
+    # TODO: Add color values to squares
+    # TODO: ensure piece names are always 3 in length: [WKi, WQu, BKi, BQu]
   end
 
-  def view
-    if @position[0].even? && @position[1].even?
-      build_view.on_black
-    elsif @position[0].even? && @position[1].odd?
-      build_view.on_white
-    elsif @position[0].odd? && @position[1].even?
-      build_view.on_white
-    elsif @position[0].odd? && @position[1].odd?
-      build_view.on_black
+  def add_edge; end
+
+  def assign_color
+    if (@position[0].even? && @position[1].even?) || (@position[0].odd? && @position[1].odd?)
+      @color = 'black'
+    elsif (@position[0].even? && @position[1].odd?) || (@position[0].odd? && @position[1].even?)
+      @color = 'white'
     end
-  end
-
-  private
-
-  def build_view
-    "      \n  #{@contents&.blue || ' '}   \n      "
   end
 end
