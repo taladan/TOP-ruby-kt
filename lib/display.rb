2511fa026d3196f98350203a587e5f954956879e
D.J. Crosby <taladan@gmail.com>
2023-05-05 21:58:10 -0400

    fix: display formatting
    display now shows board and contents correctly

--- lib/display.rb
+++ lib/display.rb
@@ -1,4 +1,5 @@
 # display.rb
+require 'colorize'
 
 class Display
   def initialize
@@ -9,43 +10,33 @@ class Display
     }
   end
 
-  # collate and return a string
-  # Each square is divided into thirds.  view {top:stuff, middle:stuff, bottom:stuff}
-  # gather the squares that belong in a row, then iterate through each square and
-  # collect their strings into an array that can be joined on the back end.
-
-  # To get all squares in a row, we can first get the square at position [0,row#], then find
-  # that square's east neighbor, then that square's east neighbor, etc. until we have no more
-  # east neighbors listed. (nil)
-  # start = [7,7]
-
-  # Once we have all the squares in that row, we can iterate through them and build their views
+  # this receives an array of square objects, and returns a string for display
+  # every square is treated as having a top, middle, and bottom pixel.  Contents of
+  # each square will be displayed only in the middle pixel of the square.
   def build_row_string(row)
-    
+    pixels = { top: '', middle: '', bottom: '' }
+    string = '      '
+    pixels.each do |pixel, _v|
+      row.each do |square|
+        pixels[:top] << set_color(string, square.color) if pixel == :top
+        if pixel == :middle && !square.contents.nil?
+          pixels[:middle] << set_color("  #{square.contents&.blue} ", square.color)
+        elsif pixel == :middle && square.contents.nil?
+          pixels[:middle] << set_color(string, square.color)
+        end
+        pixels[:bottom] << set_color(string, square.color) if pixel == :bottom
+      end
+    end
+    [pixels[:top], pixels[:middle], pixels[:bottom]].join("\n")
   end
 
   private
 
-  def recursively_build_row(square, output = [])
-    # Assume square is first in its row
-    return output << square if square.neighbors[:e].nil?
-
-    output << square
-    next_square = @board.find_square_by_name(square.neighbors[:e])
-    recursively_build_row(next_square, output)
-  end
-
-  def build_view(_square)
-    # This will all likely change
+  def set_color(string, color)
     if color == 'black'
-      @view.top = (' ' * 6).on_black
-      @view.middle = "  #{@square.contents&.blue || ' '} ".on_black
-      @viewbottom = (' ' * 6).on_black
-    elsif color == 'white'
-      top = (' ' * 6).on_white
-      middle = "  #{@square.contents&.blue || ' '} ".on_white
-      bottom = (' ' * 6).on_white
+      string.on_black
+    else
+      string.on_white
     end
-    [top, middle, bottom].join("\n")
   end
 end
