4d6a8b9a2adcd15758660196e2f33617266e36e4
D.J. Crosby <taladan@gmail.com>
2023-05-07 18:06:03 -0400

    fix: modify conditions

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -7,15 +7,20 @@ class ChessPiece
   def initialize(name, color, current_square)
     raise ArgumentError, 'Invalid Name' unless %w[K Q B N R P].include?(name)
 
-    @name = name
     @color = color
+    @name = name
     @current_square = current_square
     @available_moves = nil
     calculate_available_moves
   end
 
   def boundary?(square)
+    # Square occupied
+    return true if square.occupied?
     # Square is out of bounds
     return true if square.position[0] < 0 || square.position[1] < 0 || square.position[0] > 7 || square.position[1] > 7
+
+    # Square is empty, proceed
+    false
   end
 end
