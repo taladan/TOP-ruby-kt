a41cce5f84099b8140268919b2592e4ecb042f97
D.J. Crosby <taladan@gmail.com>
2023-05-07 17:59:21 -0400

    feat: add boundary checking and some instance vars

--- lib/chess_piece.rb
+++ lib/chess_piece.rb
@@ -4,10 +4,18 @@
 class ChessPiece
   attr_accessor :color, :current_square
 
-  def initialize
-    @name = nil
-    @color = nil
-    @current_square = nil
+  def initialize(name, color, current_square)
+    raise ArgumentError, 'Invalid Name' unless %w[K Q B N R P].include?(name)
+
+    @name = name
+    @color = color
+    @current_square = current_square
     @available_moves = nil
+    calculate_available_moves
+  end
+
+  def boundary?(square)
+    # Square is out of bounds
+    return true if square.position[0] < 0 || square.position[1] < 0 || square.position[0] > 7 || square.position[1] > 7
   end
 end
