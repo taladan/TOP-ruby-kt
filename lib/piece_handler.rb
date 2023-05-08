02d23636bfade8650d263852f892dfec47bfb53e
D.J. Crosby <taladan@gmail.com>
2023-05-08 10:17:37 -0400

    chore: clean up raised errors

--- lib/piece_handler.rb
+++ lib/piece_handler.rb
@@ -25,14 +25,14 @@ module Pieces
 
   # move a piece from a named square to a named square ('a1-h8')
   def move_piece(from_square, to_square)
-    raise 'Invalid Starting Position' unless @valid_squares.include?(from_square)
-    raise 'Invalid Ending Position' unless @valid_squares.include?(to_square)
+    raise ArgumentError, 'Invalid Starting Position' unless @valid_squares.include?(from_square)
+    raise ArgumentError, 'Invalid Ending Position' unless @valid_squares.include?(to_square)
 
     # load squares
     from = find_square_by_name(from_square)
     to = find_square_by_name(to_square)
 
-    raise 'Empty Square' if from.contents.nil?
+    raise ArgumentError, 'No piece detected in starting Square' if from.contents.nil?
 
     # swap squares contents
     to.contents = from.contents
