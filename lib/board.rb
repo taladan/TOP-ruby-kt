9a16a22f96aa4b3cc671b1d509702e5677190d6c
D.J. Crosby <taladan@gmail.com>
2023-05-08 09:34:54 -0400

    chore: organize code by alpha

--- lib/board.rb
+++ lib/board.rb
@@ -23,21 +23,6 @@ class Board
 
   private
 
-  # create board of squares return nil
-  def generate_board
-    # make squares
-    @valid_squares = combine_columns_and_rows.to_a
-    @valid_squares.each { |square| @squares << Square.new(square) }
-    # gives the squares positions in space
-    assign_square_positions
-    # calculate neighbors and assign a color to square
-    @squares.each do |square|
-      assign_neighbors(square)
-      square.assign_color
-    end
-    nil
-  end
-
   # is the queried square a boundary square?
   def boundary?(square)
     # Square occupied
@@ -49,6 +34,16 @@ class Board
 
     # Square is empty, proceed
     false
+  end
+
+  # return array of lettered columns from a - zz
+  def column_labels
+    alphas = ('a'..'zz').to_a
+    array = []
+    @columns.times do |column|
+      array << alphas[column]
+    end
+    array
   end
 
   # return an array of formatted column and row names - ex `a1 - h8`
@@ -64,23 +59,19 @@ class Board
     array
   end
 
-  # return array of lettered columns from a - zz
-  def column_labels
-    alphas = ('a'..'zz').to_a
-    array = []
-    @columns.times do |column|
-      array << alphas[column]
+  # create board of squares return nil
+  def generate_board
+    # make squares
+    @valid_squares = combine_columns_and_rows.to_a
+    @valid_squares.each { |square| @squares << Square.new(square) }
+    # gives the squares positions in space
+    assign_square_positions
+    # calculate neighbors and assign a color to square
+    @squares.each do |square|
+      assign_neighbors(square)
+      square.assign_color
     end
-    array
-  end
-
-  # create an array of row numbers from 1
-  def make_rows
-    array = []
-    @rows.times do |row|
-      array << row + 1
-    end
-    array
+    nil
   end
 
   # generate a 2d array where the first element iterates [0,0], [1,0], [2,0], [3,0]...etc.
@@ -97,5 +88,14 @@ class Board
     row = position[1]
     col = position[0]
     rows.include?(row) && cols.include?(col)
+  end
+
+  # create an array of row numbers from 1
+  def make_rows
+    array = []
+    @rows.times do |row|
+      array << row + 1
+    end
+    array
   end
 end
